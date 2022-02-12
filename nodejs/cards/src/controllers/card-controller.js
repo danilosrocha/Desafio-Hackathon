13ee83395d282b7db046bd164b10435bf103485d
Danilo Rocha <danilosilvasrocha@gmail.com>
2022-02-12 14:27:48 -0300

    Atualização das funções, faltando apenas a última exigida

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -6,13 +6,7 @@ api.findAll = (request, response) => {
     .find({})
     .sort({ name: 1 })
     .exec((exception, cards) => {
-      if (exception) {
-        const setence = "Unable to LIST";
-        console.error(setence, exception);
-
-        response.status(exception.status | 400);
-        response.json({ mensagem: setence });
-      }
+      
       console.log("Card LISTED Successfully", cards);
       response.json(cards);
     });
@@ -21,13 +15,7 @@ api.findAll = (request, response) => {
 api.save = (request, response) => {
   const cardData = request.body;
   neDB.insert(cardData, (exception, cards) => {
-    if (exception) {
-      const setence = "Could not SAVE";
-      console.error(setence, exception);
 
-      response.status(exception.status | 400);
-      response.json({ mensagem: setence });
-    }
     console.log("Card data SAVED successfully", cards);
     response.status(201);
     response.json(cards);
@@ -36,14 +24,8 @@ api.save = (request, response) => {
 
 api.update = (request, response) => {
   cardData = request.body;
-  neDB.update({ _id: request.params.id }, { cardData }, (exception, cards) => {
-    if (exception) {
-      const setence = "Could not UPDATE";
-      console.error(setence, exception);
-
-      response.status(exception.status | 400);
-      response.json({ mensagem: setence });
-    }
+  neDB.update({ _id: request.params.id }, {cardData}, (exception, cards) => {
+    
     console.log("Card data UPDATE successfully", cards);
     response.json(cards);
   });
@@ -51,31 +33,22 @@ api.update = (request, response) => {
 
 api.delete = (request, response) => {
   neDB.remove({ _id: request.params.id }, {}, (exception, cards) => {
-    if (exception) {
-      const setence = "Could not REMOVE";
-      console.error(setence, exception);
-
-      response.status(exception.status | 400);
-      response.json({ mensagem: setence });
-    }
+    
     console.log("Card data REMOVE successfully", cards);
     response.json(cards);
   });
 };
 
 api.findOne = (request, response) => {
-  neDB
-    .findOne({ _id: request.params.id })
-    .sort({ name: 1 })
-    .exec((exception, cards) => {
-      if (exception) {
+  neDB.findOne({ _id: request.params.id }).sort({ name: 1 }).exec((exception, cards) => {
+      if (cards == null) {
         const setence = "Unable to LIST by ID";
-        console.error(setence, exception);
-
-        response.status(exception.status | 400);
-        response.json({ mensagem: setence });
-      }
-      console.log("Successfully LISTED by ID", cards);
+        console.error(setence);
+        response.status(404);
+      } else {
+        console.log("Successfully LISTED by ID", cards);
+        
+      };
       response.json(cards);
     });
 };
